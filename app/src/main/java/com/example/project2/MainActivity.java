82191cae385323a7e3938242b1448ba7faf25bf5
ponc3138 <fponce@csumb.edu>
2023-04-26 16:46:21 -0700

    Initial commit

---
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.project2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    Button createAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        createAccount = binding.createAccountButton;
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Code to execute when button is clicked
+                Intent intent = new Intent(getApplicationContext(), New_account_activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
